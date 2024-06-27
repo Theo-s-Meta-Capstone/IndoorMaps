c8c96317940c325abafdd40c6e0b39d1436c0f13
Theodore Halpern <theohal@meta.com>
2024-06-26 20:38:58 -0700

    inprogress: data loads but ts types arn't working yet

---
+++ IndoorMapsFrontend/src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,94 @@
+/**
+ * @generated SignedSource<<76573623b9f0db5af80401b81f51d9fe>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type AppQuery$variables = Record<PropertyKey, never>;
+export type AppQuery$data = {
+  readonly allUsers: ReadonlyArray<{
+    readonly email: string;
+  }>;
+};
+export type AppQuery = {
+  response: AppQuery$data;
+  variables: AppQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "email",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "allUsers",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "allUsers",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "18671817be3d45ac0be9ec5bdf5aa180",
+    "id": null,
+    "metadata": {},
+    "name": "AppQuery",
+    "operationKind": "query",
+    "text": "query AppQuery {\n  allUsers {\n    email\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "c3903d7d11e8134b391bf2405b229eb4";
+
+export default node;
