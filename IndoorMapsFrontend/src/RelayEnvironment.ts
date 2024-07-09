5467549760e79a23080c33882160b1f0a1d28a7e
Theodore Halpern <theohal@meta.com>
2024-07-08 17:27:32 -0700

    feat: added basic frontend support for the test subscription

--- IndoorMapsFrontend/src/RelayEnvironment.ts
+++ IndoorMapsFrontend/src/RelayEnvironment.ts
@@ -4,9 +4,20 @@ import {
   RecordSource,
   Store,
   FetchFunction,
+  RequestParameters,
+  Variables,
+  Observable,
 } from "relay-runtime";
+import { createClient } from 'graphql-ws';
 
 const HTTP_ENDPOINT = import.meta.env.VITE_BACKEND_GRAPHQL_URL;
+
+// Subscription from https://the-guild.dev/graphql/ws/recipes#client-usage-with-relay
+const WS_ENDPOINT = 'ws://localhost:4000/graphql';
+
+const subscriptionsClient = createClient({
+  url: WS_ENDPOINT,
+});
 
 const fetchFn: FetchFunction = async (request, variables) => {
   const resp = await fetch(HTTP_ENDPOINT, {
@@ -27,9 +38,35 @@ const fetchFn: FetchFunction = async (request, variables) => {
   return await resp.json();
 };
 
+// supposedly both fetch and subscribe can be handled through one implementation
+// but fetdhing doesn not work on this website with this function
+// leaving this note here for TODO further invistagation
+// to understand why we return Observable<any>, please see: https://github.com/enisdenjo/graphql-ws/issues/316#issuecomment-1047605774
+function fetchOrSubscribe(
+  operation: RequestParameters,
+  variables: Variables,
+): Observable<any> {
+  return Observable.create((sink) => {
+    if (!operation.text) {
+      return sink.error(new Error('Operation text cannot be empty'));
+    }
+    return subscriptionsClient.subscribe(
+      {
+        operationName: operation.name,
+        query: operation.text,
+        variables,
+      },
+      sink,
+    );
+  });
+}
+
+
 function createRelayEnvironment() {
   return new Environment({
-    network: Network.create(fetchFn),
+    // supposedly this could be Network.create(fetchOrSubscribe, fetchOrSubscribe), see above comments
+    // but it doesn't work
+    network: Network.create(fetchFn, fetchOrSubscribe),
     store: new Store(new RecordSource()),
   });
 }
--- IndoorMapsFrontend/src/components/routes/BuildingViewer.tsx
+++ IndoorMapsFrontend/src/components/routes/BuildingViewer.tsx
@@ -1,10 +1,11 @@
 import "./BuildingViewer/BuildingViewer.css"
-import { Suspense, useEffect } from "react";
-import { PreloadedQuery, graphql, usePreloadedQuery, useQueryLoader } from "react-relay";
+import { Suspense, useEffect, useMemo } from "react";
+import { PreloadedQuery, graphql, usePreloadedQuery, useQueryLoader, useSubscription } from "react-relay";
 import { useParams } from "react-router-dom";
 import { BuildingViewerQuery } from "./__generated__/BuildingViewerQuery.graphql";
 import BuildingViewerBody from "./BuildingViewer/BuildingViewerBody";
 import HeaderNav from "../pageSections/HeaderNav";
+import { BuildingViewerSubscription } from "./__generated__/BuildingViewerSubscription.graphql";
 
 const BuildingViewerPageQuery = graphql`
     query BuildingViewerQuery($data: BuildingUniqueInput!) {
@@ -17,6 +18,18 @@ const BuildingViewerPageQuery = graphql`
     }
 }`
 
+const subscription = graphql`
+  subscription BuildingViewerSubscription {
+  newLiveLocation {
+    id
+    latitude
+    longitude
+    name
+    message
+  }
+}
+`;
+
 const BuildingViewer = () => {
     const { buildingId } = useParams();
 
@@ -26,6 +39,27 @@ const BuildingViewer = () => {
     ] = useQueryLoader<BuildingViewerQuery>(
         BuildingViewerPageQuery,
     );
+
+    // IMPORTANT: your config should be memoized.
+    // Otherwise, useSubscription will re-render too frequently.
+    const config = useMemo(() => ({
+        variables: {},
+        subscription,
+        // a callback that is executed when a subscription payload is received
+        onNext: (res: any) => {
+            // In res, we get the updated reference that, we can use in
+            // GroupLessonsPage to read updated data from relay store.
+            console.log(res); // here we will get the fragment Reference
+        },
+        // a callback that is executed when the subscription errors.
+        onError: (err: any) => {
+            console.error(err)
+        },
+        // a callback that is executed when the server ends the subscription
+        onCompleted: () => { },
+    }), [subscription]);
+
+    useSubscription<BuildingViewerSubscription>(config);
 
     // See ./Root.tsx line 24 for explanation of this useEffect
     // TODO: load the correct building based off the params
@@ -57,10 +91,10 @@ type BuildingViewerBodyContainerProps = {
 }
 
 function BuildingViewerBodyContainer({ queryReference }: BuildingViewerBodyContainerProps) {
-    const {getUserFromCookie, getBuilding} = usePreloadedQuery(BuildingViewerPageQuery, queryReference);
+    const { getUserFromCookie, getBuilding } = usePreloadedQuery(BuildingViewerPageQuery, queryReference);
     return (
         <>
-            <HeaderNav getUserFromCookie={getUserFromCookie} pageTitle={getBuilding.title} currentPage={"/"}/>
+            <HeaderNav getUserFromCookie={getUserFromCookie} pageTitle={getBuilding.title} currentPage={"/"} />
             <BuildingViewerBody buildingFromParent={getBuilding} />
         </>
     )
