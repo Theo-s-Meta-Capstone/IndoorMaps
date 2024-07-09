0e3e7fa6078f402efd2e3d8010e527210bc2aba9
Theodore Halpern <theohal@meta.com>
2024-07-09 12:55:43 -0700

    WIP: getting the playload length

--- IndoorMapsBackend/src/utils/websocketServer.ts
+++ IndoorMapsBackend/src/utils/websocketServer.ts
@@ -1,22 +1,29 @@
 import net from 'net';
 import crypto from 'crypto';
 
+// based on https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers#decoding_payload_length
+// WIP
+const getPayloadLength = (data: Buffer): number => {
+    console.log(data[1])
+    return 0;
+}
+
 // implementation based on https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
 export const server = net.createServer(sock => {
     let client = sock.remoteAddress;
     console.log('serving stream to ' + client);
 
-    sock.on('data', data => {
-        const dataString = data.toString();
-        const dataLines = dataString.split("\r\n");
-        console.log(dataLines);
+    const estiblishWsConnection = (dataLines: string[]) => {
         let userWebsocketKey: string = "";
+        let cookiesHeader: string = "";
         dataLines.forEach(line => {
             if (line.startsWith("sec-websocket-key: ")) {
                 userWebsocketKey = line.split(" ")[1];
             }
-        }
-        )
+            else if (line.startsWith("cookie: ")) {
+                cookiesHeader = line.split(" ")[1];
+            }
+        })
 
         console.log("sec-websocket-key request = " + userWebsocketKey)
         let magicString = userWebsocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
@@ -26,14 +33,16 @@ export const server = net.createServer(sock => {
         sock.write(
             `HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${magicString}\r\n\r\n`
         )
+    }
+
+    sock.on('data', data => {
+        const dataString = data.toString();
+        const dataLines = dataString.split("\r\n");
+        if (dataLines[0] == "GET /ws HTTP/1.1") {
+            estiblishWsConnection(dataLines);
+            return;
+        }
+        console.log(data);
+        getPayloadLength(data);
     });
-
-    // // Headers:
-    // sock.write(
-    //     'HTTP/1.0 200 OK\r\n' +
-    //     '\r\n'
-    // );
-
-    // sock.write('Hello world');
-    // sock.end(); // HTTP 1.0 signals end of packet by closing the socket
 });
