118dfb0b5cc71bf1a08dba1f5138ce68dbb00ea5
Theo <theomhalpern@gmail.com>
2024-07-10 21:39:50 -0700

    style: added logo to header

--- IndoorMapsFrontend/src/components/pageSections/HeaderNav.tsx
+++ IndoorMapsFrontend/src/components/pageSections/HeaderNav.tsx
@@ -2,7 +2,7 @@ import "./style/HeaderNav.css"
 import { Group } from "@mantine/core";
 import ButtonsContainer from "./ButtonsContainer";
 import { ButtonsContainerFragment$key } from "./__generated__/ButtonsContainerFragment.graphql";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 type HeaderNavProps = {
     getUserFromCookie: ButtonsContainerFragment$key;
@@ -14,7 +14,10 @@ type HeaderNavProps = {
 const HeaderNav = ({ getUserFromCookie, pageTitle, children }: HeaderNavProps) => {
     return (<>
         <header>
-            <h1 className="pageTitle">{pageTitle}</h1>
+            <h1 className="pageTitle" onClick={() => Navigate({ to: "/" })}>
+                <img className="pageTitleImage" src={"/logo.svg"} />
+                {pageTitle}
+            </h1>
             <Group component="nav" className="nav">
                 <Link to="/">Home</Link>
                 <Link to="/directory">Directory</Link>
