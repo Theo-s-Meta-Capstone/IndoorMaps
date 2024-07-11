2e2d15b253e855dd50a2c4a09b0daf3d5a38c9b9
Theo <theomhalpern@gmail.com>
2024-07-11 08:40:13 -0700

    fix: the building logo in the header links to the home page

--- IndoorMapsFrontend/src/components/pageSections/HeaderNav.tsx
+++ IndoorMapsFrontend/src/components/pageSections/HeaderNav.tsx
@@ -2,7 +2,7 @@ import "./style/HeaderNav.css"
 import { Group } from "@mantine/core";
 import ButtonsContainer from "./ButtonsContainer";
 import { ButtonsContainerFragment$key } from "./__generated__/ButtonsContainerFragment.graphql";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type HeaderNavProps = {
     getUserFromCookie: ButtonsContainerFragment$key;
@@ -14,8 +14,10 @@ type HeaderNavProps = {
 const HeaderNav = ({ getUserFromCookie, pageTitle, children }: HeaderNavProps) => {
     return (<>
         <header>
-            <h1 className="pageTitle" onClick={() => Navigate({ to: "/" })}>
-                <img className="pageTitleImage" src={"/logo.svg"} />
+            <h1 className="pageTitle">
+                <Link to={"/"}>
+                    <img alt={"Home Page"} className="pageTitleImage" src={"/logo.svg"} />
+                </Link>
                 {pageTitle}
             </h1>
             <Group component="nav" className="nav">
