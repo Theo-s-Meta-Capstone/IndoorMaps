5f454cd49aa35999584435a62c5b995b25efffe3
Theo <theomhalpern@gmail.com>
2024-07-23 09:40:59 -0700

    perf: replaced for each loop with for loop in dijkstra

--- IndoorMapsBackend/src/navMesh/NavigateWithNavMesh.ts
+++ IndoorMapsBackend/src/navMesh/NavigateWithNavMesh.ts
@@ -1,7 +1,6 @@
 import { LatLng } from "../graphqlSchemaTypes/Building";
-import { NavMesh, NavMeshVertex } from "./GenerateNavMesh.js";
+import { NavMesh } from "./GenerateNavMesh.js";
 import { PriorityQueue } from "./PriorityQueue.js";
-import { getDistanceBetweenGPSPoints } from "./helpers.js";
 
 // based on https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
 /**
@@ -11,33 +10,30 @@ import { getDistanceBetweenGPSPoints } from "./helpers.js";
  * @param toIndex the index of the vertex to end at in the navMesh
  * @returns [an Array of NavMeshVertex that represent the shortest path from fromIndex to toIndex, the distance between fromIndex and toIndex in km]
  */
-const dijkstra = (navMesh: NavMesh, fromIndex: number, toIndex: number): [NavMeshVertex[], number] => {
+const dijkstra = (navMesh: NavMesh, fromIndex: number, toIndex: number): [LatLng[], number] => {
     const minQueue = new PriorityQueue<number>([fromIndex]);
     const distance: { [key: number]: number } = {};
+    for (let i = 0, n = navMesh.length; i < n; i++) {
+        distance[i] = Infinity;
+    }
     distance[fromIndex] = 0;
     const previousNearestVertex: { [key: number]: number | undefined } = {};
-    navMesh.forEach((vertex, i) => {
-        if (i !== fromIndex) {
-            distance[i] = Infinity;
-            previousNearestVertex[i] = undefined;
-        }
-    })
     while (minQueue.size > 0) {
         const nextClosest = minQueue.poll()!;
-        navMesh[nextClosest].edges.forEach((vertex) => {
+        for (const vertex of navMesh[nextClosest].edges) {
             const alt = distance[nextClosest] + vertex.weight
             if (alt < distance[vertex.index]) {
                 previousNearestVertex[vertex.index] = nextClosest
                 distance[vertex.index] = alt
                 minQueue.addWithPriority(vertex.index, alt)
             }
-        })
+        }
     }
-    const res: NavMeshVertex[] = [];
+    const res: LatLng[] = [];
     let cur = toIndex;
-    while(previousNearestVertex[cur] !== undefined) {
+    while (previousNearestVertex[cur] !== undefined) {
         // It's weired that TS makes me do ! here, it should know that it can't be undefined
-        res.push(navMesh[previousNearestVertex[cur]!]);
+        res.push(navMesh[previousNearestVertex[cur]!].point);
         cur = previousNearestVertex[cur]!;
     }
     return [res.reverse(), distance[toIndex]];
@@ -51,10 +47,9 @@ const dijkstra = (navMesh: NavMesh, fromIndex: number, toIndex: number): [NavMes
  * @returns an array of latlng that can be desplayed as a line to show the path from fromIndex to toIndex, the distance between fromIndex and toIndex in km
  */
 export const findShortestPath = (navMesh: NavMesh, fromIndex: number, toIndex: number): [LatLng[], number] => {
-    const res: LatLng[] = [];
     const [path, distance] = dijkstra(navMesh, fromIndex, toIndex)
-    if(path.length === 0) return [[], -1]
-    res.push(...path.map((vertex) => vertex.point))
+    if (path.length === 0) return [[], -1]
+    const res = path
     res.push(navMesh[toIndex].point)
     return [res, distance];
 }
